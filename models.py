6b7a4e5998e98606cb97ce478f411eedbdedaa54
Nipun Agarwala <nipun.agarwala@stanford.edu>
2017-05-19 16:03:57 -0700

    Pushed changes to models.py

--- models.py
+++ models.py
@@ -21,136 +21,207 @@ class Config():
 		self.keep_prob = 0.8
 
 
-	class RCNN_RNN(object)
+class RCNN_RNN(object)
 
-		def __init__(self, features_shape, num_classes, cell_type='lstm', seq_len, reuse=False, add_bn=False,
-					add_reg=False, scope=None):
-			self.config = Config()
-			self.config.features_shape = features_shape
-			self.config.num_classes = num_encodings+1
-			self.reuse = reuse
-			self.inputs_placeholder = tf.placeholder(tf.float32, shape=(None,None,)+ self.config.features_shape )
-			self.targets_placeholder = tf.placeholder(tf.int32, shape=(None,) + self.targets_shape)
-			self.config.seq_len = seq_len
+	def __init__(self, features_shape, num_classes, cell_type='lstm', seq_len, reuse=False, add_bn=False,
+				add_reg=False, scope=None):
+		self.config = Config()
+		self.config.features_shape = features_shape
+		self.config.num_classes = num_encodings+1
+		self.reuse = reuse
+		self.inputs_placeholder = tf.placeholder(tf.float32, shape=(None,None,)+ self.config.features_shape )
+		self.targets_placeholder = tf.placeholder(tf.int32, shape=(None,) + self.targets_shape)
+		self.config.seq_len = seq_len
 
-			self.scope = scope
-			if add_bn:
-				self.norm_fn = tf.contrib.layers.batch_norm
-			else
-				self.norm_fn = None
+		self.scope = scope
+		if add_bn:
+			self.norm_fn = tf.contrib.layers.batch_norm
+		else
+			self.norm_fn = None
 
-			if add_reg:
-				self.reg_fn = tf.nn.l2_loss
-			else
-				self.reg_fn = None
+		if add_reg:
+			self.reg_fn = tf.nn.l2_loss
+		else
+			self.reg_fn = None
 
-			if cell_type == 'rnn':
-				self.cell = tf.contrib.rnn.RNNCell
-			elif cell_type == 'gru':
-				self.cell = tf.contrib.rnn.GRUCell
-			elif cell_type == 'lstm':
-				self.cell = tf.contrib.rnn.LSTMCell
-			else:
-				raise ValueError('Input correct cell type')
+		if cell_type == 'rnn':
+			self.cell = tf.contrib.rnn.RNNCell
+		elif cell_type == 'gru':
+			self.cell = tf.contrib.rnn.GRUCell
+		elif cell_type == 'lstm':
+			self.cell = tf.contrib.rnn.LSTMCell
+		else:
+			raise ValueError('Input correct cell type')
 
 
-		def build_cnn(self, cur_inputs):
-			conv_out1 = tf.contrib.layers.conv2d(inputs=cur_inputs, num_outputs=32, kernel_size=[3,3],
+	def build_cnn(self, cur_inputs):
+		conv_out1 = tf.contrib.layers.conv2d(inputs=cur_inputs, num_outputs=32, kernel_size=[3,3],
+							stride=[1,1],padding='SAME',rate=1,activation_fn=tf.nn.relu,
+							normalizer_fn=self.norm_fn,	weights_initializer=XAVIER_INIT ,
+							weights_regularizer=self.reg_fn , biases_regularizer=self.reg_fn ,
+							reuse = self.reuse, trainable=True)
+
+		conv_out2 = tf.contrib.layers.conv2d(inputs=conv_out1, num_outputs=32, kernel_size=[3,3],
+							stride=[1,1],padding='SAME',rate=1,activation_fn=tf.nn.relu,
+							normalizer_fn=self.norm_fn,	weights_initializer=XAVIER_INIT ,
+							weights_regularizer=self.reg_fn , biases_regularizer=self.reg_fn ,
+							reuse = self.reuse, trainable=True)
+
+		max_pool1 = tf.contrib.layers.max_pool2d(inputs=conv_out2, kernel_size=[3,3],stride=[1,1],padding='SAME')
+
+		conv_out3 = tf.contrib.layers.conv2d(inputs=max_pool1, num_outputs=32, kernel_size=[3,3],
+							stride=[1,1],padding='SAME',rate=1,activation_fn=tf.nn.relu,
+							normalizer_fn=self.norm_fn,	weights_initializer=XAVIER_INIT ,
+							weights_regularizer=self.reg_fn , biases_regularizer=self.reg_fn ,
+							reuse = self.reuse, trainable=True)
+
+		conv_out4 = tf.contrib.layers.conv2d(inputs=conv_out3, num_outputs=32, kernel_size=[3,3],
+							stride=[1,1],padding='SAME',rate=1,activation_fn=tf.nn.relu,
+							normalizer_fn=self.norm_fn,	weights_initializer=XAVIER_INIT ,
+							weights_regularizer=self.reg_fn , biases_regularizer=self.reg_fn ,
+							reuse = self.reuse, trainable=True)
+
+		max_pool2 = tf.contrib.layers.max_pool2d(inputs=conv_out4, kernel_size=[3,3],stride=[1,1],padding='SAME')
+
+		cnn_out = max_pool2
+		return cnn_out
+
+	def build_rnn(self, rnn_inputs):
+		W = tf.get_variable("Weights", shape=[self.config.hidden_size, self.config.num_classes],
+							initializer=XAVIER_INIT)
+		b = tf.get_variable("Bias", shape=[self.config.num_classes])
+
+		rnnNet = tf.contrib.rnn.MultiRNNCell([self.cell(num_units = self.config.hidden_size) for _ in 
+									range(self.config.num_layers)], state_is_tuple=True)
+		(rnnNet_out, rnnNet_state) = tf.nn.dynamic_rnn(cell = rnnNet, inputs=rnn_inputs,
+		                sequence_length=self.config.seq_len,dtype=tf.float32)
+
+		cur_shape = tf.shape(rnnNet_out)
+		rnnOut_2d = tf.reshape(rnnNet_out, [-1, cur_shape[2]])
+
+		logits_2d = tf.matmul(rnnOut_2d, W) + b
+		rnn_out = tf.reshape(logits_2d,[cur_shape[0], cur_shape[1], self.config.num_classes])
+
+		return rnn_out
+
+	def build_model(self):
+		self.cnn_scope = self.scope + '/CNN'
+		cnn_outputs = []
+		with tf.variable_scope(self.cnn_scope):
+			for t in xrange(self.seq_len):
+				if t > 0:  tf.get_variable_scope().reuse_variables()
+				cnn_outputs.append(self.build_cnn(self.inputs_placeholder[t,:,:,:,:]))
+
+		cnn_outputs = tf.stack(cnn_outputs, axis=1)
+
+		self.rnn_scope = self.scope + '/RNN'
+		rnn_output = None
+		with tf.variable_scope(self.rnn_scope):
+			rnn_output = self.build_rnn(cnn_outputs)
+
+		self.logits = rnn_output
+
+
+	def add_loss_op(self):
+
+	def add_optimizer_op(self):
+		tvars = tf.trainable_variables()
+		grads, _ = tf.clip_by_global_norm(tf.gradients(self.loss, tvars),self.config.max_norm)
+		optimizer = tf.train.AdamOptimizer(self.config.lr)
+		self.train_op = optimizer.apply_gradients(zip(grads, tvars))
+
+
+	def add_error_op(self):
+
+	def add_summary_op(self):
+		self.merged_summary_op = tf.summary.merge_all()
+
+
+	def add_feed_dict(self, input_batch, target_batch, seq_batch):
+		feed_dict = {self.inputs_placeholder:input_batch, self.targets_placeholder:target_batch,
+						self.seq_lens_placeholder:seq_batch}
+		return feed_dict
+
+
+	def train_one_batch(self, session, input_batch, target_batch, seq_batch):
+		feed_dict = self.add_feed_dict(input_batch, target_batch, seq_batch)
+
+
+	def test_one_batch(self, session):
+
+	def get_config(self):
+		return self.config
+
+
+class VisualAttention(object)
+
+	def __init__(self, features_shape, num_classes, cell_type='lstm', seq_len, reuse=False, add_bn=False,
+				add_reg=False, scope=None):
+		self.config = Config()
+		self.config.features_shape = features_shape
+		self.config.num_classes = num_classes
+		self.reuse = reuse
+		self.inputs_placeholder = tf.placeholder(tf.float32, shape=(None,)+ self.config.features_shape )
+		self.targets_placeholder = tf.placeholder(tf.int32, shape=(None,) + self.targets_shape)
+		self.config.seq_len = seq_len
+		self.emission_num_layers =  1
+
+		self.scope = scope
+		if add_bn:
+			self.norm_fn = tf.contrib.layers.batch_norm
+		else
+			self.norm_fn = None
+
+		if add_reg:
+			self.reg_fn = tf.nn.l2_loss
+		else
+			self.reg_fn = None
+
+		if cell_type == 'rnn':
+			self.cell = tf.contrib.rnn.RNNCell
+		elif cell_type == 'gru':
+			self.cell = tf.contrib.rnn.GRUCell
+		elif cell_type == 'lstm':
+			self.cell = tf.contrib.rnn.LSTMCell
+		else:
+			raise ValueError('Input correct cell type')
+
+
+	def context_network(self, scope):
+		with tf.variable_scope(scope):
+			conv_out1 = tf.contrib.layers.conv2d(inputs=self.inputs_placeholder , num_outputs=32, kernel_size=[3,3],
 								stride=[1,1],padding='SAME',rate=1,activation_fn=tf.nn.relu,
 								normalizer_fn=self.norm_fn,	weights_initializer=XAVIER_INIT ,
 								weights_regularizer=self.reg_fn , biases_regularizer=self.reg_fn ,
 								reuse = self.reuse, trainable=True)
-
 			conv_out2 = tf.contrib.layers.conv2d(inputs=conv_out1, num_outputs=32, kernel_size=[3,3],
 								stride=[1,1],padding='SAME',rate=1,activation_fn=tf.nn.relu,
 								normalizer_fn=self.norm_fn,	weights_initializer=XAVIER_INIT ,
 								weights_regularizer=self.reg_fn , biases_regularizer=self.reg_fn ,
 								reuse = self.reuse, trainable=True)
-
-			max_pool1 = tf.contrib.layers.max_pool2d(inputs=conv_out2, kernel_size=[3,3],stride=[1,1],padding='SAME')
-
-			conv_out3 = tf.contrib.layers.conv2d(inputs=max_pool1, num_outputs=32, kernel_size=[3,3],
+			conv_out3 = tf.contrib.layers.conv2d(inputs=conv_out2, num_outputs=32, kernel_size=[3,3],
 								stride=[1,1],padding='SAME',rate=1,activation_fn=tf.nn.relu,
 								normalizer_fn=self.norm_fn,	weights_initializer=XAVIER_INIT ,
 								weights_regularizer=self.reg_fn , biases_regularizer=self.reg_fn ,
 								reuse = self.reuse, trainable=True)
-
-			conv_out4 = tf.contrib.layers.conv2d(inputs=conv_out3, num_outputs=32, kernel_size=[3,3],
-								stride=[1,1],padding='SAME',rate=1,activation_fn=tf.nn.relu,
-								normalizer_fn=self.norm_fn,	weights_initializer=XAVIER_INIT ,
-								weights_regularizer=self.reg_fn , biases_regularizer=self.reg_fn ,
-								reuse = self.reuse, trainable=True)
-
-			max_pool2 = tf.contrib.layers.max_pool2d(inputs=conv_out4, kernel_size=[3,3],stride=[1,1],padding='SAME')
-
-			cnn_out = max_pool2
-			return cnn_out
-
-		def build_rnn(self, rnn_inputs):
-			W = tf.get_variable("Weights", shape=[self.config.hidden_size, self.config.num_classes],
-								initializer=XAVIER_INIT)
-			b = tf.get_variable("Bias", shape=[self.config.num_classes])
-
-			rnnNet = tf.contrib.rnn.MultiRNNCell([self.cell(num_units = self.config.hidden_size) for _ in 
-										range(self.config.num_layers)], state_is_tuple=True)
-			(rnnNet_out, rnnNet_state) = tf.nn.dynamic_rnn(cell = rnnNet, inputs=rnn_inputs,
-			                sequence_length=self.config.seq_len,dtype=tf.float32)
-
-			cur_shape = tf.shape(rnnNet_out)
-			rnnOut_2d = tf.reshape(rnnNet_out, [-1, cur_shape[2]])
-
-			logits_2d = tf.matmul(rnnOut_2d, W) + b
-			rnn_out = tf.reshape(logits_2d,[cur_shape[0], cur_shape[1], self.config.num_classes])
-
-			return rnn_out
-
-		def build_model(self):
-			self.cnn_scope = self.scope + '/CNN'
-			cnn_outputs = []
-			with tf.variable_scope(self.cnn_scope):
-				for t in xrange(self.seq_len):
-					if t > 0:  tf.get_variable_scope().reuse_variables()
-					cnn_outputs.append(self.build_cnn(self.inputs_placeholder[t,:,:,:,:]))
-
-			cnn_outputs = tf.stack(cnn_outputs, axis=1)
-
-			self.rnn_scope = self.scope + '/RNN'
-			rnn_output = None
-			with tf.variable_scope(self.rnn_scope):
-				rnn_output = self.build_rnn(cnn_outputs)
-
-			self.logits = rnn_output
+			self.context_net_out = tf.contrib.layers.flatten(conv_out3)
 
 
-		def add_loss_op(self):
+	def glimpse_network(self, scope):
+		with tf.variable_scope(scope):
 
-		def add_optimizer_op(self):
-			tvars = tf.trainable_variables()
-			grads, _ = tf.clip_by_global_norm(tf.gradients(self.loss, tvars),self.config.max_norm)
-			optimizer = tf.train.AdamOptimizer(self.config.lr)
-			self.train_op = optimizer.apply_gradients(zip(grads, tvars))
-
-
-		def add_error_op(self):
-
-		def add_summary_op(self):
-			self.merged_summary_op = tf.summary.merge_all()
+	def emission_network(self, scope):
+		with tf.variable_scope(scope):
+			emission_cell = tf.contrib.rnn.MultiRNNCell([self.cell(num_units = self.config.hidden_size) for _ in 
+										range(self.emission_num_layers)], state_is_tuple=True)
+			(self.emission_out, self.emission_state) = tf.nn.dynamic_rnn(cell = emission_cell, 
+							inputs=tf.zeros_like(self.context_net_out), sequence_length=self.config.seq_len+1,
+							initial_state=self.context_net_out,dtype=tf.float32)
 
 
-		def add_feed_dict(self, input_batch, target_batch, seq_batch):
-			feed_dict = {self.inputs_placeholder:input_batch, self.targets_placeholder:target_batch,
-							self.seq_lens_placeholder:seq_batch}
-			return feed_dict
 
-
-		def train_one_batch(self, session, input_batch, target_batch, seq_batch):
-			feed_dict = self.add_feed_dict(input_batch, target_batch, seq_batch)
-
-
-		def test_one_batch(self, session):
-
-		def get_config(self):
-			return self.config
+	def classification_network(self, scope):
+		with tf.variable_scope(scope):
 
 
 
