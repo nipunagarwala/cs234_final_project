a9bb6f909169a1a8ed860daae0a596be9c9b6b87
Priyanka <pnigam@users.noreply.github.com>
2017-06-06 21:10:42 +0000

    Add multi0object visualization.

--- utils_data/visualize.py
+++ utils_data/visualize.py
@@ -3,30 +3,49 @@ import os
 import cPickle as pickle
 import numpy as np
 
-mean_pixel = [ 116.45473115, 112.27971193, 106.51900657] # VOT
+mean_pixel = [114.78968156, 114.81268655, 113.29880948] # [ 116.45473115, 112.27971193, 106.51900657] # VOT
 orig_shape = (180, 320)
-# VTB
+# VTB # 114.78968156  114.81268655  113.29880948]
 
 def generate_images(sequence, predicted_bboxes, true_bboxes, save_path, basename):
-    i = 0
     mean_pixel_array = np.ones_like(sequence[0], dtype='float64')
     mean_pixel_array[:, :, 0] *= mean_pixel[0]
     mean_pixel_array[:, :, 1] *= mean_pixel[1]
     mean_pixel_array[:, :, 2] *= mean_pixel[2]
-    for s in sequence:
+    for i in range(len(sequence)):
+        s = sequence[i]
         img = np.uint8(s + mean_pixel)
         img = Image.fromarray(img)
         predicted_bbox = predicted_bboxes[i]
         true_bbox = true_bboxes[i]
         predicted_bbox = [predicted_bbox[1] * orig_shape[1], predicted_bbox[0] * orig_shape[0], (predicted_bbox[3]+predicted_bbox[1])* orig_shape[1], (predicted_bbox[2]+predicted_bbox[0])* orig_shape[0]]
         true_bbox = [true_bbox[1] * orig_shape[1], true_bbox[0] * orig_shape[0], (true_bbox[3]+true_bbox[1])* orig_shape[1], (true_bbox[2]+true_bbox[0])* orig_shape[0]]
-        print predicted_bbox, true_bbox
         
         draw = ImageDraw.Draw(img)
         draw.rectangle(predicted_bbox)
         draw.rectangle(true_bbox, outline=(255,0,0,255))
         img.save(os.path.join(save_path, basename + str(i) +".png"))
-        i += 1
+
+def generate_multi_object_images(sequence, predicted_bboxes, true_bboxes, save_path, basename):
+    mean_pixel_array = np.ones_like(sequence[0], dtype='float64')
+    mean_pixel_array[:, :, 0] *= mean_pixel[0]
+    mean_pixel_array[:, :, 1] *= mean_pixel[1]
+    mean_pixel_array[:, :, 2] *= mean_pixel[2]
+    for i in range(len(sequence)):
+        s = sequence[i]
+        img = np.uint8(s + mean_pixel)
+        img = Image.fromarray(img)
+        draw = ImageDraw.Draw(img)
+        
+        for j in range(len(predicted_bboxes)):
+            predicted_bbox = predicted_bboxes[j][i]
+            true_bbox = true_bboxes[j][i]
+            predicted_bbox = [predicted_bbox[1] * orig_shape[1], predicted_bbox[0] * orig_shape[0], (predicted_bbox[3]+predicted_bbox[1])* orig_shape[1], (predicted_bbox[2]+predicted_bbox[0])* orig_shape[0]]
+            true_bbox = [true_bbox[1] * orig_shape[1], true_bbox[0] * orig_shape[0], (true_bbox[3]+true_bbox[1])* orig_shape[1], (true_bbox[2]+true_bbox[0])* orig_shape[0]]
+            
+            draw.rectangle(predicted_bbox)
+            draw.rectangle(true_bbox, outline=(255,0,0,255))
+        img.save(os.path.join(save_path, basename + str(i) +".png"))
 
 if __name__ == "__main__":
     filename = "/data/vtb/data/test/BlurBody-0001"
@@ -38,3 +57,13 @@ if __name__ == "__main__":
         pred_labels[:, 0] += 0.1
         pred_labels[:, 1] += 0.1
         generate_images(curr_seq, pred_labels, curr_labels, "/data/testvisualize", "BlurBody")
+        
+        curr_labels = [curr_labels, pred_labels]
+        pred_labels_obj1 = np.copy(pred_labels)
+        pred_labels_obj1[:, 0] += 0.1
+        pred_labels_obj1[:, 1] += 0.1
+        pred_labels_obj2 = np.copy(pred_labels_obj1)
+        pred_labels_obj2[:, 0] += 0.1
+        pred_labels_obj2[:, 1] += 0.1
+        pred_labels = [pred_labels_obj1, pred_labels_obj2]
+        generate_multi_object_images(curr_seq, pred_labels, curr_labels, "/data/testvisualize", "MultiBlurBody")
